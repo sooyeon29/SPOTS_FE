8e86c30114ad621ad227d25f2080e74b10bf2dea
sooyun <so__oyeonii@naver.com>
2022-11-07 17:02:12 +0900

    📂doc: 메인페이지 & 상세페이지 파일구조 변경 pages>mainpage/reservation/spotsDetail 확인하기

--- src/pages/reservation/Index.jsx
+++ src/pages/reservation/Index.jsx
@@ -1,13 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
 import Layout from "../../components/Layout";
 
 const Reservation = () => {
+  const navigate = useNavigate();
   return (
     <>
-    <Layout>
-      <Header />
-    <div>hi</div>
-    </Layout>
+      <Layout>
+        <Header />
+        <div>hi</div>
+        <button onClick={() => navigate(`/spotsdetail`)}>상세페이지로</button>
+      </Layout>
     </>
   );
 };
