71d6d15bd677acf6100dc6ab4b8b45c06519b7bd
goeunrain <boniteterain@gmail.com>
2022-11-07 15:46:58 +0900

    🛠 feat : 라우터 수정, 예약현황토글

--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 import Login from "../pages/login/Index";
-import MainMaps from "../pages/mainpage/MainMaps";
+import MainMaps from "../pages/mainpage/Index";
 import SpotsDetail from "../pages/spotsDetail";
 import UserPage from "../pages/userpage/Index";
 import TeamDetail from "../pages/userpage/TeamDetail";
+import Reservation from "../pages/reservation/Index";
 
 function Router() {
   return (
@@ -19,6 +20,7 @@ function Router() {
         <Route path="/userpage" element={<UserPage />} />
         <Route path="/spotsdetail" element={<SpotsDetail />} />
         <Route path="/teamdetail" element={<TeamDetail />} />
+        <Route path="/book" element={<Reservation />} />
       </Routes>
     </BrowserRouter>
   );
