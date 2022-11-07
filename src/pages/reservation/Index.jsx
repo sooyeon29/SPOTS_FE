ec2701b93b32c8adc2c9cbfc6cf9650618ae03ce
Mirae Kim <miraekim1031@gmail.com>
2022-11-07 08:13:09 -0500

    ⚒️: 검색창 추가

--- src/pages/reservation/Index.jsx
+++ src/pages/reservation/Index.jsx
@@ -1,13 +1,35 @@
-import Header from "../../components/Header";
-import Layout from "../../components/Layout";
+import { useState } from 'react';
+import Header from '../../components/Header';
+import Layout from '../../components/Layout';
 
 const Reservation = () => {
+  const [search, setSearch] = useState();
+  const onChangeSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+  
+  const onSearch = (e) => {
+    e.preventDefault();
+    
+  }
+
   return (
     <>
-    <Layout>
-      <Header />
-    <div>hi</div>
-    </Layout>
+      <Layout>
+        <Header />
+        <div>
+          <form onSubmit={(e) => onSearch(e)}>
+            <input
+              type='text'
+              value={search}
+              placeholder='구를 입력하세요 예) 마포구'
+              onChange={onChangeSearch}
+            />
+            <button type='submit'>스팟 찾기</button>
+          </form>
+        </div>
+      </Layout>
     </>
   );
 };
