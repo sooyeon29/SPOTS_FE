64ea8a8ed49214cb7a84b558a8db25de8305c256
sooyun <so__oyeonii@naver.com>
2022-11-08 23:37:06 +0900

    [fix] 지도에 마커찍기 카카오리액트라이브러리 사용

--- src/pages/mainpage/Maps2.jsx
+++ src/pages/mainpage/Maps2.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { placesInfo } from "../../tools/dummy";
 
 const { kakao } = window;
 
@@ -13,32 +14,51 @@ const Maps2 = () => {
     const map = new kakao.maps.Map(container, options);
     // 주소-좌표 변환 객체를 생성합니다.
     const geocoder = new kakao.maps.services.Geocoder();
-    // 주소로 좌표를 검색합니다..
-    geocoder.addressSearch(
-      "서울 용산구 한강대로23길 55 용산아이파크몰",
-      function (result, status) {
-        // 정상적으로 검색이 완료됐으면
+
+    placesInfo.forEach(function (placeInfo) {
+      geocoder.addressSearch(placeInfo.place, function (result, status) {
         if (status === kakao.maps.services.Status.OK) {
           let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-          // 결과값으로 받은 위치를 마커로 표시합니다
           let marker = new kakao.maps.Marker({
             map: map,
             position: coords,
           });
-
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
           let infowindow = new kakao.maps.InfoWindow({
             content:
-              '<div style="width:150px;color:red;text-align:center;padding:6px 0;">풋살장</div>',
+              '<div style="width:150px;color:red;text-align:center;padding:6px 0;">' +
+              placeInfo.name +
+              "</div>",
           });
           infowindow.open(map, marker);
-
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
           map.setCenter(coords);
         }
-      }
-    );
+      });
+      // 주소로 좌표를 검색합니다..
+      // geocoder.addressSearch(
+      //   "서울 용산구 한강대로23길 55 용산아이파크몰",
+      //   function (result, status) {
+      //     // 정상적으로 검색이 완료됐으면
+      //     if (status === kakao.maps.services.Status.OK) {
+      //       let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+      //       // 결과값으로 받은 위치를 마커로 표시합니다
+      //       let marker = new kakao.maps.Marker({
+      //         map: map,
+      //         position: coords,
+      //       });
+
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      //   let infowindow = new kakao.maps.InfoWindow({
+      //     content:
+      //       '<div style="width:150px;color:red;text-align:center;padding:6px 0;">풋살장</div>',
+      //   });
+      //   infowindow.open(map, marker);
+
+      //   // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      //   map.setCenter(coords);
+      // }
+    });
   }, []);
 
   return (
--- src/shared/router.jsx
+++ src/shared/router.jsx
@@ -10,7 +10,7 @@ import TeamRegister from "../pages/userpage/TeamRegister";
 import Reservation from "../pages/reservation/Index";
 import Maps2 from "../pages/mainpage/Maps2";
 import Kakao from "../pages/login/Kakao";
-
+import ReactMap from "../pages/mainpage/Maps3";
 
 function Router() {
   return (
@@ -25,6 +25,7 @@ function Router() {
         <Route path="/teamregister" element={<TeamRegister />} />
         <Route path="/book" element={<Reservation />} />
         <Route path="/map" element={<Maps2 />} />
+        <Route path="/maptest" element={<ReactMap />} />
         <Route path="/auth/kakao/callback" element={<Kakao />} />
       </Routes>
     </BrowserRouter>
