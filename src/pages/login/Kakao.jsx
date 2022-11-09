b07a132c33a8eccfc3bfa0a79c7c33d9784789b6
Mirae Kim <miraekim1031@gmail.com>
2022-11-09 11:58:44 -0500

    [feat] 카카오소셜로그인 프론트단 구현

--- src/pages/login/Kakao.jsx
+++ src/pages/login/Kakao.jsx
@@ -4,36 +4,59 @@ import { useDispatch } from "react-redux";
 import { LoginAPI } from "../../tools/instance";
 
 const Kakao = () => {
-  console.log(window.location.href);
-  useEffect(() => {
-    // 인가코드
-    const PARAMS = new URL(document.location).searchParams;
-    const KAKAO_CODE = PARAMS.get("code");
-    // console.log(KAKAO_CODE);
-    let grant_type = "authorization_code";
-    let client_id =
-      // "497bb40725964bac025412acbaf9fc7c";
-      "3da70995df25a93c7655852b81c50a54";
+  const REST_API_KEY = "d3e20268980676f1708f48a456b8c297";
+  const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+  const CODE = new URL(document.location).searchParams;
+  const KAKAOCODE = CODE.get("code");
+  console.log(KAKAOCODE);
 
-    axios
-      .post(
-        `https://kauth.kakao.com/oauth/token?
-      grant_type=${grant_type}
-      &client_id=${client_id}
-      &redirect_uri=http://localhost:3000/auth/kakao/callback
-      &code=${KAKAO_CODE}`,
-        {
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-          },
-        }
-      )
-      // LoginAPI.kakaoLogin(KAKAO_CODE)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  const getKakaoToken = () => {
+    fetch(`https://kauth.kakao.com/oauth/token`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAOCODE}`,
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+  }
+
+  useEffect(() => {
+    getKakaoToken();
+  }, [])
+
+
+  // useEffect(() => {
+  //   // 인가코드
+  //   const PARAMS = new URL(document.location).searchParams;
+  //   const KAKAO_CODE = PARAMS.get("code");
+  //   // console.log(KAKAO_CODE);
+  //   let grant_type = "authorization_code";
+  //   let client_id =
+  //     // "497bb40725964bac025412acbaf9fc7c";
+  //     "3da70995df25a93c7655852b81c50a54";
+
+  //   axios
+  //     .post(
+        // `https://kauth.kakao.com/oauth/token?
+  //     grant_type=${grant_type}
+  //     &client_id=${client_id}
+  //     &redirect_uri=http://localhost:3000/auth/kakao/callback
+  //     &code=${KAKAO_CODE}`,
+  //       {
+  //         headers: {
+  //           "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+  //         },
+  //       }
+  //     )
+  //     // LoginAPI.kakaoLogin(KAKAO_CODE)
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
 };
 
 export default Kakao;
