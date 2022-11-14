3696219c5d7468232c9c5e144a98bccf2f8ca936
goeunrain <boniteterain@gmail.com>
2022-11-14 12:18:19 +0900

    [doc] 파일명 수정

---
+++ src/pages/login/Index.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Layout from "../../components/Layout";
+import { LoginAPI } from "../../tools/instance";
+import { KAKAO_AUTH_URL } from "./OAuth";
+import { StWraps, Stinput, KakaoBtn } from "./styles";
+
+const Login = () => {
+  const [loginInfo, setLoginInfo] = useState({
+    id: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const idAndPassword = (e) => {
+    const { name, value } = e.target;
+    setLoginInfo({ ...loginInfo, [name]: value });
+  };
+  console.log("인풋창 잘 들어오나", loginInfo);
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    LoginAPI.login({ loginId: loginInfo.id, password: loginInfo.password })
+      .then((res) => {
+        console.log("로그인성공 response", res);
+        if (res.status === 200) {
+          localStorage.setItem("token", res.data.accessToken);
+          navigate("/");
+          window.location.reload();
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          alert("이미 로그인 상태입니다.");
+        } else if (err.response.status === 412) {
+          alert("아이디 또는 패스워드를 확인해주세요");
+        }
+        console.log("로그인실패시 err", err);
+      });
+  };
+
+  return (
+    <>
+      <Layout>
+        <StWraps>
+          <h1>로그인</h1>
+          <form onSubmit={loginHandler}>
+            <div>
+              <div>
+                <Stinput
+                  placeholder="아이디를 입력하세요."
+                  type="text"
+                  required
+                  name="id"
+                  onChange={idAndPassword}
+                />
+              </div>
+              <div>
+                <Stinput
+                  placeholder="비밀번호를 입력하세요."
+                  type="password"
+                  required
+                  name="password"
+                  onChange={idAndPassword}
+                />
+              </div>
+            </div>
+            <button>로그인</button>
+            <div>
+              아직 회원이 아니신가요? <a href="/signup"> 회원가입</a>
+            </div>
+          </form>
+          {/* 소셜로그인 - 카카오로그인 */}
+          <KakaoBtn>
+            {/* // href="https://ws-study.shop/auth/kakao"> */}
+            <img alt="" src="/kakao.png" width={30} />
+            <a href={KAKAO_AUTH_URL}>카카오계정 로그인</a>
+          </KakaoBtn>
+        </StWraps>
+      </Layout>
+    </>
+  );
+};
+export default Login;
---
+++ src/pages/login/Styles.jsx
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import { lighten, darken } from "polished";
+
+export const StWraps = styled.div`
+  > h1 {
+    font-weight: bold;
+  }
+
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  text-align: center;
+  form {
+    button {
+      height: 52px;
+      padding: 11px 0px 10px;
+      background: #8fc7ff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      border: 0px;
+      border-radius: 8px;
+      cursor: pointer;
+      font-weight: bold;
+      font-size: 16px;
+      line-height: 24px;
+      margin-top: 32px;
+      &:hover {
+        background-color: ${darken(0.1, "#8fc7ff")};
+      }
+    }
+  }
+`;
+
+export const Stinput = styled.input`
+  background-color: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0 30px 0;
+  width: 100%;
+  min-width: 300px;
+  font-family: "MonoplexKR-Regular";
+`;
+export const KakaoBtn = styled.button`
+  height: 52px;
+  padding: 11px 0px 10px;
+  background: #ffcd2a;
+  display: flex;
+  align-items: center;
+
+  justify-content: center;
+  width: 100%;
+  border: 0px;
+  border-radius: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 32px;
+  &:hover {
+    background-color: ${lighten(0.1, "#ffcd2a")};
+  }
+  img {
+    margin: auto 10px;
+  }
+`;
