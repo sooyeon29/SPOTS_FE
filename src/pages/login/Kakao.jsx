742f0453690b2e018a453e41982f603d7cf58622
sooyun <so__oyeonii@naver.com>
2022-11-16 00:09:24 +0900

    [feat]: 카카오 소셜로그인 기능 성공

--- src/pages/login/Kakao.jsx
+++ src/pages/login/Kakao.jsx
@@ -4,29 +4,31 @@ import { useDispatch } from "react-redux";
 import { LoginAPI } from "../../tools/instance";
 
 const Kakao = () => {
-  const REST_API_KEY = "d3e20268980676f1708f48a456b8c297";
-  const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
-  const CODE = new URL(document.location).searchParams;
-  const KAKAOCODE = CODE.get("code");
-  console.log(KAKAOCODE);
+  // const REST_API_KEY = "d3e20268980676f1708f48a456b8c297";
+  // const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+  // const CODE = new URL(document.location).searchParams;
+  // const KAKAOCODE = CODE.get("code");
+  // console.log(KAKAOCODE);
 
-  const getKakaoToken = () => {
-    fetch(`https://kauth.kakao.com/oauth/token`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAOCODE}`,
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
-  }
+  // const getKakaoToken = () => {
+  //   fetch(`https://kauth.kakao.com/oauth/token`, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAOCODE}`,
+  //   })
+  //     .then((res) =>
+  //       // console.log(res))
+  //       res.json()
+  //     )
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // };
 
-  useEffect(() => {
-    getKakaoToken();
-  }, [])
-
-
+  // useEffect(() => {
+  //   getKakaoToken();
+  // }, []);
+  // ===========================================================================위코드 주석풀면 토큰받기까지ok
   // useEffect(() => {
   //   // 인가코드
   //   const PARAMS = new URL(document.location).searchParams;
@@ -39,7 +41,7 @@ const Kakao = () => {
 
   //   axios
   //     .post(
-        // `https://kauth.kakao.com/oauth/token?
+  // `https://kauth.kakao.com/oauth/token?
   //     grant_type=${grant_type}
   //     &client_id=${client_id}
   //     &redirect_uri=http://localhost:3000/auth/kakao/callback
@@ -56,7 +58,24 @@ const Kakao = () => {
   //     })
   //     .catch((err) => console.log(err));
   // }, []);
+  // ==================================================================================아래 수연코드
+  console.log(window.location.href);
 
+  // 인가코드
+  const PARAMS = new URL(document.location).searchParams;
+  const KAKAO_CODE = PARAMS.get("code");
+  console.log(KAKAO_CODE);
+  useEffect(() => {
+    LoginAPI.kakaoLogin(KAKAO_CODE)
+      .then((res) => {
+        console.log(res);
+        // localStorage.setItem("token", JSON.stringify(res.data));
+
+        // localStorage.setItem("kakaocode", KAKAO_CODE);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return <>로그인 성공이라구!!!</>;
 };
 
 export default Kakao;
