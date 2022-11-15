3266f0d997d12ee31cfed7db0b52f619c0b14b4f
goeunrain <boniteterain@gmail.com>
2022-11-15 22:22:29 +0900

    [feat] socket 서버와 연결 전

--- src/tools/instance.jsx
+++ src/tools/instance.jsx
@@ -5,8 +5,8 @@ const isLogin = localStorage.getItem("token");
 // const isKakaoLogin = localStorage.getItem("kakaocode");
 
 const instance = axios.create({
-  baseURL: "https://ws-study.shop/",
-  // baseURL: "https://sparta4.shop/",
+  // baseURL: "https://ws-study.shop/",
+  baseURL: "https://sparta4.shop/",
   // baseURL: "http://localhost:3000/",
   // baseURL: "http://13.125.53.34/",
   headers: {
