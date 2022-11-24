0f8cdc03147172766e50c53c3b0a7f2e044475a5
goeunrain <boniteterain@gmail.com>
2022-11-24 21:41:26 +0900

    [feat] alert 처리, 구장 등록 오류 수정

--- src/pages/login/SwitchAccount.jsx
+++ src/pages/login/SwitchAccount.jsx
@@ -3,6 +3,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { UserpageAPI } from "../../tools/instance";
 import Header from "../../components/Header";
 import Layout from "../../components/Layout";
+import Swal from "sweetalert2";
+import styled from "styled-components";
 
 const SwitchAccount = () => {
   const navigate = useNavigate();
@@ -13,39 +15,53 @@ const SwitchAccount = () => {
     <>
       <Layout>
         <Header />
-        <div>SPOTS 휴면계정 전환 안내</div>
-        <div>
-          <p>{loginId}님의 계정은 현재 휴면 상태입니다.</p>
-        </div>
-        <button
-          onClick={() => {
-            UserpageAPI.switchMe()
-              .then((res) => {
-                console.log(res);
-                if (res.status === 200) {
-                  alert("계정이 활성화되었습니다. 로그인 페이지로 이동합니다.");
-                  localStorage.clear();
-                  navigate("/login");
-                }
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          }}
-        >
-          휴면 해제하기
-        </button>
-        <button
-          onClick={() => {
-            localStorage.clear();
-            navigate("/login");
-          }}
-        >
-          취소
-        </button>
+        <Container>
+          <div>SPOTS 휴면계정 전환 안내</div>
+          <div>
+            <p>{loginId}님의 계정은 현재 휴면 상태입니다.</p>
+          </div>
+          <button
+            onClick={() => {
+              UserpageAPI.switchMe()
+                .then((res) => {
+                  console.log(res);
+                  if (res.status === 200) {
+                    Swal.fire({
+                      text: "계정이 활성화되었습니다. 로그인 페이지로 이동합니다.",
+                      width: "300px",
+                      confirmButtonText: "확인",
+                      confirmButtonColor: "#40d295",
+                      showClass: { popup: "animated fadeInDown faster" },
+                      hideClass: { popup: "animated fadeOutUp faster" },
+                    });
+                    localStorage.clear();
+                    navigate("/login");
+                  }
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          >
+            휴면 해제하기
+          </button>
+          <button
+            onClick={() => {
+              localStorage.clear();
+              navigate("/login");
+            }}
+          >
+            취소
+          </button>
+        </Container>
       </Layout>
     </>
   );
 };
 
 export default SwitchAccount;
+
+const Container = styled.div`
+  padding-top: 100px;
+  padding-left: 30px;
+`;
