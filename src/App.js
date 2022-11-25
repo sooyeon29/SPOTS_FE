c2d300dc77e4ddeef0e9d14dee7ea37f11b0b3f5
sooyun <so__oyeonii@naver.com>
2022-11-25 20:37:30 +0900

    [fix]배포환경콘솔안보이게숨김

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import Router from "./shared/router";
 
 function App() {
   // 배포 환경에서 console.log, console.warn 지우기
-  // if (process.env.NODE_ENV === "production") {
-  //   console.log = function no_console() {};
-  //   console.warn = function no_console() {};
-  // }
+  if (process.env.NODE_ENV === "production") {
+    console.log = function no_console() {};
+    console.warn = function no_console() {};
+  }
   return (
     <>
       <Router />
