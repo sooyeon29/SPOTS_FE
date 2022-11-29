507859d7aee2ec3af68cea49d0a8f0f7bd056220
sooyun <so__oyeonii@naver.com>
2022-11-29 14:28:29 +0900

    [feat]: 새토큰으로 갈아끼워지는 인터셉터ok / 요청이 두번가는것으로 보임->새로고침을해야됨

--- src/tools/instance.jsx
+++ src/tools/instance.jsx
@@ -31,30 +31,59 @@ instance.interceptors.request.use(
   }
 );
 // // 응답 인터셉터 추가
+// instance.interceptors.response.use(
+//   async (response) => {
+//     // 응답 데이터가 있는 작업 수행
+//     console.log("인터셉터리스판스+++++++++++++++++:", response);
+//     if (response.status === 200 && response.data.code === 1) {
+//       window.localStorage.removeItem("token");
+//       window.localStorage.setItem("token", response.data.myNewToken);
+//       const newAccessToken = response.data.myNewToken;
+//       return axios({
+//         ...response.config,
+//         headers: {
+//           Authorization: `${newAccessToken}`,
+//         },
+//       }).then((res) => console.log("토큰잘 바뀐건가아아ㅏ아", res));
+//     }
+//     return response;
+//   },
+//   (error) => {
+//     if (error.status === 401) {
+//       Swal.fire({
+//         text: "로그인 시간이 만료되었습니다. 다시 로그인해주세요!",
+//         width: "300px",
+//         confirmButtonText: "확인",
+//         confirmButtonColor: "#40d295",
+//         showClass: { popup: "animated fadeInDown faster" },
+//         hideClass: { popup: "animated fadeOutUp faster" },
+//       });
+//       window.location.replace("/login");
+//     }
+//     Promise.reject(error);
+//   }
+// );
+
 instance.interceptors.response.use(
   (response) => {
-    // 응답 데이터가 있는 작업 수행
-    console.log("인터셉터리스판스+++++++++++++++++:", response);
-    if (response.status === 200 && response.data.code === 1) {
-      window.localStorage.removeItem("token");
-      window.localStorage.setItem("token", response.data.myNewToken);
-      const newAccessToken = response.data.myNewToken;
-      return axios({
-        ...response.config,
-        headers: {
-          Authorization: `${newAccessToken}`,
-        },
-      }).then((res) => console.log("토큰잘 바뀐건가아아ㅏ아", res));
-      // instance.interceptors.request.use((config) => {
-      //   // 요청이 전달되기 전 작업 수행
-      //   // console.log("인터셉터리퀘스트:", config);
-      //   console.log("여기도찍히나");
-      //   const token = localStorage.getItem("token");
-      //   if (token) {
-      //     config.headers["Authorization"] = token;
-      //   }
-      //   return config;
-      // });
+    console.log("★Axios interceptors response execute★");
+    let firstToken = localStorage.getItem("token");
+
+    if (firstToken) {
+      console.log("----------------------------------------------");
+      console.log("첫토큰", firstToken);
+      response.headers["Authorization"] = response.data.myNewToken;
+
+      if (response.data.myNewToken) {
+        console.log("신규토큰", response.data.myNewToken);
+
+        if (firstToken !== response.data.myNewToken) {
+          firstToken = response.data.myNewToken;
+          window.localStorage.setItem("token", response.data.myNewToken);
+          console.log("토큰정보 업데이트!!");
+        }
+      }
+      console.log("----------------------------------------------");
     }
     return response;
   },
