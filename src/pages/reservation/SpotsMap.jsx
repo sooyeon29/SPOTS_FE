01ddddd4b44f5eae9da6fccd42024aeeb9de7996
goeunrain <boniteterain@gmail.com>
2022-12-08 18:23:31 +0900

    [fix] mypage 수정

--- src/pages/reservation/SpotsMap.jsx
+++ src/pages/reservation/SpotsMap.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { BtnWrap, Container } from "./Style";
+import { BtnWrap, Container } from "./Styles";
 import {
   Map,
   ZoomControl,
