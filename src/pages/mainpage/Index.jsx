d271a82f9ec1aeadb905871b52683d518e7c3877
sooyun <so__oyeonii@naver.com>
2022-12-08 16:45:49 +0900

    [style] 메인 style파일 최종

--- src/pages/mainpage/Index.jsx
+++ src/pages/mainpage/Index.jsx
@@ -5,12 +5,30 @@ import Header from "../../components/Header";
 import Layout from "../../components/Layout";
 import TapBar from "../../components/TapBar";
 import useDetectClose from "../../hooks/useDetectClose";
-import styled from "styled-components";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ChatBtn from "../../components/ChatBtn";
 import ChatRoom from "../chat/ChatRoom";
+import {
+  BannerSlider,
+  Icon,
+  Image,
+  Info,
+  Info2,
+  InfoDiv,
+  LastTime,
+  MainBanner,
+  MainSearch,
+  New,
+  Section,
+  SixMatch,
+  SpotContainer,
+  SpotInfoMain,
+  SpotName,
+  TeamContainer,
+  WaitingMatchMain,
+  WaitingMatchMain2,
+} from "./Styles";
 
 const MainMaps = () => {
   const [newSpot, setNewSpot] = useState();
@@ -156,166 +174,3 @@ const MainMaps = () => {
 };
 
 export default MainMaps;
-
-const MainSearch = styled.img`
-  width: 100%;
-  margin-top: 60px;
-`;
-
-const MainBanner = styled.div`
-  margin: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  img {
-    width: 98%;
-  }
-`;
-
-const SpotContainer = styled.div`
-  overflow: hidden;
-  margin-top: 20px;
-`;
-
-const Section = styled.div`
-  margin: 15px 0px 8px 15px;
-  font-size: 18px;
-  font-weight: 900;
-  font-family: SpoqaHanSansNeoBold;
-`;
-
-const BannerSlider = styled(Slider)`
-  .slick-slide div {
-    /* border-radius: 10px; */
-    margin: 0px 10px 0px 0px;
-  }
-  .slick-dots {
-    color: red;
-  }
-  cursor: pointer;
-`;
-
-const New = styled.div`
-  width: 80%;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 200px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  box-shadow: 0px 20px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 80px;
-`;
-
-const InfoDiv = styled.div`
-  background-color: #fff;
-  padding: 10px;
-  width: 6.41%;
-  border-radius: 10px;
-  position: absolute;
-  top: 60%;
-  box-shadow: 0px 20px 10px rgba(0, 0, 0, 0.1);
-  display: flex;
-`;
-
-const Info = styled.div`
-  width: 200px;
-`;
-
-const Icon = styled.img`
-  height: 25px;
-  margin-bottom: 5px;
-`;
-
-const SpotName = styled.div`
-  font-weight: bold;
-  font-size: 18px;
-`;
-
-const TeamContainer = styled.div`
-  margin-bottom: 65px;
-`;
-
-export const SixMatch = styled.div`
-  background-color: #f7f8f8;
-  width: 95%;
-  box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  margin: 15px auto;
-  padding-top: 10px;
-  padding-left: 25px;
-`;
-
-const WaitingMatchMain = styled.div`
-  font-weight: bold;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 95%;
-  border-radius: 10px;
-  margin: 3px;
-  padding: 2px;
-  font-size: 15px;
-  div {
-    display: flex;
-    align-items: center;
-    img {
-      margin: 0px 10px 10px 0px;
-    }
-    span {
-      margin: 0px 10px 10px 0px;
-    }
-  }
-`;
-const LastTime = styled.div`
-  background-color: #ff00b4;
-  color: white;
-  border-radius: 10px;
-  padding: 5px 7px;
-  font-size: 13px;
-  margin-right: 10px;
-`;
-const WaitingMatchMain2 = styled(WaitingMatchMain)`
-  div {
-    width: 50%;
-  }
-`;
-
-const SpotInfoMain = styled.div`
-  background-color: white;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  width: 95%;
-  margin-bottom: 10px;
-
-  img {
-    width: 70px;
-    height: 50px;
-    object-fit: cover;
-    margin: 0px 10px 0px 0px;
-    border-radius: 10px;
-  }
-`;
-
-const Info2 = styled.div`
-  width: 100%;
-  padding: 0px 10px;
-  button {
-    font-size: 18px;
-    font-weight: bold;
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    color: #000;
-  }
-  div {
-    margin-left: 6px;
-  }
-`;
