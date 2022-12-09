d6ac8f02c2464060a1543bbe494c4d3401f20a19
Mirae Kim <miraekim1031@gmail.com>
2022-12-09 11:38:25 -0500

    [fix] 병합 오류 해결

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -6,9 +6,9 @@ const Tutorial = () => {
   const settings = {
     dots: false, // 캐러셀이미지가 몇번째인지 알려주는 점을 보여줄지 정한다.
     // infinite: true, // loop를 만들지(마지막 이미지-처음 이미지-중간 이미지들-마지막 이미지)
-    speed: 300, // 애니메이션의 속도, 단위는 milliseconds
-    autoplaySpeed: 5000,
-    autoplay: true,
+    // speed: 300, // 애니메이션의 속도, 단위는 milliseconds
+    // autoplaySpeed: 5000,
+    // autoplay: true,
     slidesToShow: 1, // 한번에 몇개의 슬라이드를 보여줄지
     slidesToScroll: 1, // 한번 스크롤시 몇장의 슬라이드를 넘길지
     // arrows: true,
