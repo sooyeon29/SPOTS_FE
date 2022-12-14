16d54279ed1419f71e3137d16c7660f85c02d167
goeunrain <boniteterain@gmail.com>
2022-12-14 19:41:10 +0900

    [style] background 설정중

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,19 +6,31 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
-      <Wrap>{children}</Wrap>
+      <BackGround>
+        <Wrap>{children}</Wrap>
+      </BackGround>
     </>
   );
 };
 
 export default Layout;
 
+const BackGround = styled.div`
+  background-image: url("/background.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  overflow: hidden;
+`;
 const Wrap = styled.div`
+  @media screen and (max-width: 420px) {
+    margin: auto;
+  }
   position: relative;
   -webkit-box-align: center;
   box-sizing: border-box;
   width: 100%;
   max-width: 420px;
-  margin: auto;
+  margin-left: 45%;
   overflow-x: hidden;
+  background-color: white;
 `;
