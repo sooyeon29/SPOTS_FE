f250add06e4d95dc4a53162c656e7bfff7061c00
sooyun <so__oyeonii@naver.com>
2022-12-14 23:12:04 +0900

    [style] 배경화면

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 const BackGround = () => {
-  return <Image alt="" src="/background.png" />;
+  return <Image alt="" src="/background.jpg" />;
 };
 export default BackGround;
 
 const Image = styled.img`
-  width: 100%;
   height: 100%;
+  width: 100%;
+  /* height: 960px; */
   object-fit: contain;
   z-index: -1;
   position: fixed;
